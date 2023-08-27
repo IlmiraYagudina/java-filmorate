a3cc7dd1d654ab9f29f84da713c05f3a37949bbc
GutImozih73 <ilmirayagudina1996@gmail.com>
2023-08-27 21:07:25 +0300

    добавить controller и model

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,70 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Slf4j
+
+@RestController
+public class FilmController {
+    private final HashMap<Integer, Film> films = new HashMap<>();
+    private int id = 0;
+
+    @ResponseBody
+    @PostMapping(value = "/films")
+    public Film create(@Valid @RequestBody Film film) {
+        filmValidation(film);
+        films.put(film.getId(), film);
+        log.info("'{}' movie was added to a library, the identifier is '{}'", film.getName(), film.getId());
+        return film;
+    }
+
+    @ResponseBody
+    @GetMapping("/films")
+    public List<Film> getFilms() {
+        log.info("There is '{}' movies in a library now", films.size());
+        return new ArrayList<>(films.values());
+    }
+
+    @ResponseBody
+    @PutMapping("/films")
+    public Film update(@Valid @RequestBody Film film) {
+        filmValidation(film);
+        if (films.containsKey(film.getId())) {
+            films.put(film.getId(), film);
+            log.info("'{}' movie was updated in a library, the identifier is '{}'", film.getName(), film.getId());
+        } else {
+            throw new ValidationException("Attempt to update non-existing movie");
+        }
+        return film;
+    }
+
+    private void filmValidation(Film film) {
+        if (film.getReleaseDate() == null ||
+                film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ValidationException("Incorrect release date");
+        }
+        if (film.getName().isEmpty() || film.getName().isBlank()) {
+            throw new ValidationException("Attempt to set an empty movie name");
+        }
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Attempt to set duration less than zero");
+        }
+        if (film.getDescription().length() > 200 || film.getDescription().length() == 0) {
+            throw new ValidationException("Description increases 200 symbols or empty");
+        }
+        if (film.getId() <= 0) {
+            film.setId(++id);
+            log.info("Incorrect movie identifier was set as '{}", film.getId());
+        }
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,71 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Slf4j
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+    private final HashMap<Integer, User> users = new HashMap<>();
+    private int id = 0;
+
+    @ResponseBody
+    @PostMapping
+    public User create(@Valid @RequestBody User user) {
+        userValidation(user);
+        users.put(user.getId(), user);
+        log.info("The user '{}' has been saved with the identifier '{}'", user.getEmail(), user.getId());
+        return user;
+    }
+
+    @ResponseBody
+    @GetMapping
+    public List<User> getUsers() {
+        log.info("The number of users: '{}'", users.size());
+        return new ArrayList<>(users.values());
+    }
+
+    @ResponseBody
+    @PutMapping
+    public User update(@Valid @RequestBody User user) {
+        userValidation(user);
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            log.info("'{}' info with identifier '{}' was updated", user.getLogin(), user.getId());
+        } else {
+            throw new ValidationException("Attempt to update non-existing user");
+        }
+        return user;
+    }
+
+    private void userValidation(User user) {
+        if (user.getBirthday().isAfter(LocalDate.now()) || user.getBirthday() == null) {
+            throw new ValidationException("Incorrect user's birthday with identifier '" + user.getId() + "'");
+        }
+        if (user.getEmail() == null || user.getEmail().isBlank() || !user.getEmail().contains("@")) {
+            throw new ValidationException("Incorrect user's email with identifier '" + user.getId() + "'");
+        }
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+            log.info("User's name with identifier '{}' was set as '{}'", user.getId(), user.getName());
+        }
+        if (user.getId() == 0 || user.getId() < 0) {
+            user.setId(++id);
+            log.info("Incorrect user identifier was set as '{}'", user.getId());
+        }
+        if (user.getLogin().isBlank() || user.getLogin().isEmpty()) {
+            throw new ValidationException("Incorrect login with user's identifier '" + user.getId() + "'");
+        }
+    }
+}
