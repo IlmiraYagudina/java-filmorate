b95cbd6e7b4c01955328242ea26bfb3e3e720f27
GutImozih73 <ilmirayagudina1996@gmail.com>
2023-10-12 23:35:57 +0300

    22

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -31,16 +31,19 @@ public class UserController {
     public User create(@Valid @RequestBody User user) {
         Validation.userValidation(user);
 //        if (users.containsKey(user.getId())) {
-////            log.debug("Email already exists");
-////            throw new ValidationException(String.format("Email %s already exists.", user.getEmail()));
-////        }
-////        log.debug("The user '{}' has been saved with the identifier '{}'", user.getEmail(), user.getId());
-////        user.setId(id);
-////        id++;
-////        users.put(user.getId(), user);
-////        return user;
+//            log.debug("Email already exists");
+//            throw new ValidationException(String.format("Email %s already exists.", user.getEmail()));
+//        }
+//        log.debug("The user '{}' has been saved with the identifier '{}'", user.getEmail(), user.getId());
+//        user.setId(id);
+//        id++;
+//        users.put(user.getId(), user);
+//        return user;
         users.put(user.getId(), user);
         log.info("The user '{}' has been saved with the identifier '{}'", user.getEmail(), user.getId());
+        user.setId(id);
+        id++;
+//        users.put(user.getId(), user);
         return user;
     }
 
