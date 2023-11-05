aa3a39fd9927293df5d5323457e2269d80cf6409
GutImozih73 <ilmirayagudina1996@gmail.com>
2023-11-05 22:11:49 +0300

    fix: add annotation

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
@@ -13,6 +14,7 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 @Builder
+@NoArgsConstructor
 public class Film {
     private Long id; // Id фильма
     @NotBlank
