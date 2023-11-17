7da13eba4fb8ed8ab8a94cea9afd543943316ad8
GutImozih73 <ilmirayagudina1996@gmail.com>
2023-11-17 17:40:18 +0300

    style: добавить пробел

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/friend/FriendDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/friend/FriendDaoImpl.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @Component
 public class FriendDaoImpl implements FriendDao {
     private final JdbcTemplate jdbcTemplate;
+
     @Autowired
     public FriendDaoImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
