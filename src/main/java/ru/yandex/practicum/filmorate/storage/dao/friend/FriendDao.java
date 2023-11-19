57891c8bd930acbc6ff7437860ae762a7cfa3324
GutImozih73 <ilmirayagudina1996@gmail.com>
2023-11-19 20:13:36 +0300

    style1

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/friend/FriendDao.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/friend/FriendDao.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.storage.dao.friend;
 
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
 
